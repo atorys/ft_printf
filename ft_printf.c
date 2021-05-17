0f46c050e7968edaae99b8b5fe12edb6fe97b880
AchioteTory <atory@student.21-school.ru>
2021-05-17 18:24:12 +0300

    new funcs

---
+++ ft_printf.c
@@ -0,0 +1,70 @@
+//
+// Created by Achiote Tory on 5/16/21.
+//
+
+#include "libft/libft.h"
+#include "ft_printf.h"
+
+typedef struct s_flags_modifications_type
+{
+	int	minus;
+	int	plus;
+	int	space;
+	int	zero;
+	int	width;
+	int	dot;
+	int star;
+	int	precision;
+	int type;
+}				t_fmt;
+
+t_fmt	ft_new_params(void)
+{
+	t_fmt	params;
+
+	params.minus = 0;
+	params.plus = 0;
+	params.space = 0;
+	params.zero = 0;
+	params.width = 0;
+	params.dot = 0;
+	params.star = 0;
+	params.precision = 0;
+	params.type = 0;
+	return (params);
+}
+
+int	flags_mods_parsing(const char **format)
+{
+	while (**format)
+}
+
+static int	str_parsing(const char *format, va_list arguments, int *printed)
+{
+	int	status;
+
+	status = 0;
+	while (*format++ && !status)
+	{
+		if (*format == '%')
+		{
+			*format++;
+			status = flags_mods_parsing(&format, );
+		}
+		ft_putchar_fd(*format, 1);
+		printed++;
+	}
+	return (0);
+}
+
+int ft_printf(const char *format, ...)
+{
+	int		printed;
+	va_list	:arguments;
+
+	printed = 0;
+	va_start(arguments, format);
+	str_parsing(format, arguments, &printed);
+	va_end(arguments);
+	return (printed);
+}
---
+++ ft_printf.h
@@ -0,0 +1,15 @@
+//
+// Created by Achiote Tory on 5/16/21.
+//
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+/*
+ * multiple ags in function . . .
+ */
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
